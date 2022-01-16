9a3ab712d761e2495708d5e7f0627540e1874748
BLBBrightly <brightly@bblb.com>
2022-01-16 09:04:16 +0800

    init

---
+++ PN_generator.py
@@ -0,0 +1,16 @@
+import math
+
+
+def isPrime(n):
+    for i in range(2, int(math.sqrt(n) + 1)):
+        if n % i == 0:
+            return False
+    return n > 1
+
+
+if __name__ == '__main__':
+    with open("PN_list.txt", 'w') as f:
+        f.write("2")
+        for n in range(3, 9999):
+            if isPrime(n):
+                f.write(',' + str(n))
---
+++ readlines.py
@@ -0,0 +1,3 @@
+text_file = open("PN_list.txt", "r")
+lines = text_file.readlines()
+print(lines)
