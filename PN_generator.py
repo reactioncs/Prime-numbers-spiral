2499349d9182c7184b2e8d5ab76c93e7f22d5c87
BLBBrightly <brightly@bblb.com>
2022-01-16 09:06:13 +0800

    add UP_TO

--- PN_generator.py
+++ PN_generator.py
@@ -1,5 +1,7 @@
 import math
 
+UP_TO = 9999
+
 
 def isPrime(n):
     for i in range(2, int(math.sqrt(n) + 1)):
@@ -11,6 +13,6 @@ def isPrime(n):
 if __name__ == '__main__':
     with open("PN_list.txt", 'w') as f:
         f.write("2")
-        for n in range(3, 9999):
+        for n in range(3, UP_TO):
             if isPrime(n):
                 f.write(',' + str(n))
