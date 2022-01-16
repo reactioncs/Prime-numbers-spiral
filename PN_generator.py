c1a59670a126ffd86374ece1a1ccbba6fa4cc5a4
H5er <h5er@h5.com>
2022-01-16 21:39:06 +0800

    change #02

--- PN_generator.py
+++ PN_generator.py
@@ -7,7 +7,7 @@ def isPrime(n):
     for i in range(2, int(math.sqrt(n) + 1)):
         if n % i == 0:
             return False
-    return n > 1
+    return True
 
 
 if __name__ == '__main__':
--- plot.py
+++ plot.py
@@ -8,12 +8,15 @@ PN_list = np.array(text_file.read().split(','), dtype="int32")
 PN_list_part = PN_list[:]
 p = PN_list_part * [np.cos(PN_list_part), np.sin(PN_list_part)]
 
+# biggest: 999983
 SIZE = 400000
+
 fig, ax = plt.subplots(figsize=(10, 10))
 ax.scatter(p[0], p[1], s=0.2, c='#fbf00f')
 ax.axis('off')
-ax.set_xlim(-SIZE, SIZE)
-ax.set_ylim(-SIZE, SIZE)
 fig.set_facecolor("k")
 fig.tight_layout()
+
+ax.set_xlim(-SIZE, SIZE)
+ax.set_ylim(-SIZE, SIZE)
 plt.show()
