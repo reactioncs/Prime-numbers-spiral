53fa38069bf05736a7382ba36bc2a0a20f7d19cf
BLBBrightly <brightly@bblb.com>
2022-01-16 09:27:03 +0800

    change apperance

--- plot.py
+++ plot.py
@@ -6,9 +6,9 @@ text_file = open("PN_list.txt", "r")
 PN_list = np.array(text_file.read().split(','), dtype="int32")
 
 p = PN_list * [np.cos(PN_list), np.sin(PN_list)]
-print(p)
 
 fig, ax = plt.subplots(figsize=(10, 10))
-ax.plot(p[0], p[1], 'ok')
+ax.scatter(p[0], p[1], s=0.1, c="white")
+ax.set_facecolor('black')
 fig.tight_layout()
 plt.show()
